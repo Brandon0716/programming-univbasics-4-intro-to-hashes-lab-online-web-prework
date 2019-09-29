1364b8c5c104469db5617611cb5860318dfe02bf
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-29 14:36:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {"pioneer" => "Traveler"} return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 def id_generator
